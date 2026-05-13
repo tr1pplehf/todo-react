7c181934898e04e30ac063a958e7d7d27f6f0fa9
tr1pplehf <danilyakimenko11@gmail.com>
2026-05-13 15:46:38 +0700

    todo-react: #2 Начальная структура проекта.

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,118 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div className="todo">
+      <h1 className="todo__title">To Do List</h1>
+      <form className="todo__form">
+        <div className="todo__field field">
+          <label
+            className="field__label"
+            htmlFor="new-task"
+          >
+            New task
+          </label>
+          <input
+            className="field__input"
+            id="new-task"
+            placeholder=" "
+            autoComplete="off"
+          />
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+        <button className="button" type="submit">Add</button>
+      </form>
+      <form className="todo__form">
+        <div className="todo__field field">
+          <label
+            className="field__label"
+            htmlFor="search-task"
+          >
+            Search task
+          </label>
+          <input
+            className="field__input"
+            id="search-task"
+            placeholder=" "
+            autoComplete="off"
+            type="search"
+          />
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      </form>
+      <div className="todo__info">
+        <div className="todo__total-tasks">Total tasks: <span>0</span></div>
+        <button className="todo__delete-all-button" type="button">Delete all</button>
+      </div>
+      <ul className="todo__list">
+        <li className="todo__item todo-item">
+          <input
+            className="todo-item__checkbox"
+            id="task-1"
+            type="checkbox"
+            checked
+          />
+          <label
+            className="todo-item__label"
+            htmlFor="task-1"
+          >
+            Task 1
+          </label>
+          <button
+            className="todo-item__delete-button"
+            aria-label="Delete"
+            title="Delete"
+          >
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15 5L5 15M5 5L15 15"
+                stroke="#757575"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+        </li>
+        <li className="todo__item todo-item">
+          <input
+            className="todo-item__checkbox"
+            id="task-2"
+            type="checkbox"
+          />
+          <label
+            className="todo-item__label"
+            htmlFor="task-2"
+          >
+            Task 2
+          </label>
+          <button
+            className="todo-item__delete-button"
+            aria-label="Delete"
+            title="Delete"
+          >
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15 5L5 15M5 5L15 15"
+                stroke="#757575"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+        </li>
+      </ul>
+      <div className="todo__empty-message"></div>
+    </div>
   )
 }
 
