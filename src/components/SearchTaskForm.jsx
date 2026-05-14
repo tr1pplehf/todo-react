553c04f4a253324b966cf455955e79c16cc07d8f
tr1pplehf <danilyakimenko11@gmail.com>
2026-05-14 14:24:49 +0700

    todo-react: #7 Сохранение состояния в localStorage.Фильтрация задач

--- src/components/SearchTaskForm.jsx
+++ src/components/SearchTaskForm.jsx
@@ -2,7 +2,8 @@ import Field from './Field'
 
 const SearchTaskForm = (props) => {
   const {
-    onSearchInput,
+    searchQuery,
+    setSearchQuery,
   } = props
 
   return (
@@ -15,7 +16,8 @@ const SearchTaskForm = (props) => {
         label="Search task"
         id="search-task"
         type="search"
-        onInput={({target}) => onSearchInput(target.value)}
+        value={searchQuery}
+        onInput={({target}) => setSearchQuery(target.value)}
       />
     </form>
   )
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import AddTaskForm from './AddTaskForm'
 import SearchTaskForm from './SearchTaskForm'
 import TodoInfo from './TodoInfo'
 import TodoList from './TodoList'
 
 const Todo = () => {
-  const [tasks, setTasks] = useState([
-    { id: 'task-1', title: 'Погладить кота', isDone: false},
-    { id: 'task-2', title: 'Купить молоко', isDone: true},
-  ])
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem('tasks')
+
+    if (savedTasks) {
+      return JSON.parse(savedTasks)
+    }
+
+    return [
+      {id: 'task-1', title: 'Погладить кота', isDone: false},
+      {id: 'task-2', title: 'Купить молоко', isDone: true}
+    ]
+  })
 
   const [newTaskTitle, setNewTaskTitle] = useState('')
+
+  const [searchQuery, setSearchQuery] = useState('')
 
   const deleteAllTasks = () => {
     const isConfirmed = confirm('Are you sure you want to delete all tasks?')
@@ -30,16 +40,12 @@ const Todo = () => {
     setTasks(
       tasks.map((task) => {
         if (task.id === taskId) {
-          return { ...task, isDone}
+          return {...task, isDone}
         }
 
         return task
       })
     )
-  }
-
-  const filterTasks = (query) => {
-    console.log(`Finding ${query}`)
   }
 
   const addTask = () => {
@@ -52,8 +58,19 @@ const Todo = () => {
 
       setTasks([...tasks, newTask])
       setNewTaskTitle('')
+      setSearchQuery('')
     }
   }
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks])
+
+
+  const clearSearchQuery = searchQuery.trim().toLowerCase()
+  const filteredTasks = clearSearchQuery.length > 0
+    ? tasks.filter(({ title}) => title.toLowerCase().includes(clearSearchQuery))
+    : null
 
   return (
     <div className="todo">
@@ -63,14 +80,18 @@ const Todo = () => {
         newTaskTitle={newTaskTitle}
         setNewTaskTitle={setNewTaskTitle}
       />
-      <SearchTaskForm onSearchInput={filterTasks} />
+      <SearchTaskForm
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
       <TodoInfo
         total={tasks.length}
-        done={tasks.filter(({ isDone }) => isDone).length}
+        done={tasks.filter(({isDone}) => isDone).length}
         onDeleteAllButtonClick={deleteAllTasks}
       />
       <TodoList
         tasks={tasks}
+        filteredTasks={filteredTasks}
         onDeleteTaskButtonClick={deleteTask}
         onTaskCompleteChange={toggleTaskComplete}
       />
