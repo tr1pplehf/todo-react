7f4ad3cd14ed2785758aa8e621512ed6e2f908dd
tr1pplehf <danilyakimenko11@gmail.com>
2026-05-15 15:48:41 +0700

    todo-react: #17 Добавление анимации появления и удаления задачи с синхронизацией ответа от сервера.

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -16,11 +16,18 @@ const TodoItem = (props) => {
     firstIncompleteTaskId,
     deleteTask,
     toggleTaskComplete,
+    disappearingTaskId,
+    appearingTaskId,
   } = useContext(TasksContext)
 
   return (
     <li
-      className={`${styles.todoItem} ${className}`}
+      className={`
+        ${styles.todoItem}
+        ${className}
+        ${disappearingTaskId === id ? styles.isDisappearing : ''}
+        ${appearingTaskId === id ? styles.isAppearing : ''}
+      `}
       ref={id === firstIncompleteTaskId ? firstIncompleteTaskRef : null}
     >
       <input
