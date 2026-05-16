bdf2d42916872530be8d129a08842441b673d7b2
tr1pplehf <danilyakimenko11@gmail.com>
2026-05-17 05:43:04 +0700

    todo-react: #20 Защита от XSS, подсветка фрагмента поиска, оптимизация перерисовки компонентов при вводе в поле ввода новой задачи.

--- src/entities/todo/ui/TodoItem/TodoItem.jsx
+++ src/entities/todo/ui/TodoItem/TodoItem.jsx
@@ -2,6 +2,7 @@ import {memo, useContext} from 'react'
 import {TasksContext} from '@/entities/todo'
 import RouterLink from '@/shared/ui/RouterLink'
 import styles from './TodoItem.module.scss'
+import {highlightCaseInsensitive} from '@/shared/utils/highlight'
 
 const TodoItem = (props) => {
   const {
@@ -18,7 +19,10 @@ const TodoItem = (props) => {
     toggleTaskComplete,
     disappearingTaskId,
     appearingTaskId,
+    searchQuery,
   } = useContext(TasksContext)
+
+  const highlightedTitle = highlightCaseInsensitive(title, searchQuery)
 
   return (
     <li
@@ -49,7 +53,7 @@ const TodoItem = (props) => {
         to={`/tasks/${id}`}
         aria-label="Task detail page"
       >
-        {title}
+        <span dangerouslySetInnerHTML={{ __html: highlightedTitle}} />
       </RouterLink>
       <button
         className={styles.deleteButton}
--- src/features/add-task/AddTaskForm.jsx
+++ src/features/add-task/AddTaskForm.jsx
@@ -8,10 +8,10 @@ const addTaskForm = (props) => {
     styles,
   } = props
 
+  const [newTaskTitle, setNewTaskTitle] = useState('')
+
   const {
     addTask,
-    newTaskTitle,
-    setNewTaskTitle,
     newTaskInputRef,
   } = useContext(TasksContext)
 
@@ -24,7 +24,10 @@ const addTaskForm = (props) => {
     event.preventDefault()
 
     if (!isNewTaskTitleEmpty) {
-      addTask(clearNewTaskTitle)
+      addTask(
+        clearNewTaskTitle,
+        () => setNewTaskTitle('')
+        )
     }
   }
 
